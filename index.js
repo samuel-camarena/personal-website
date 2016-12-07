4ca02da7a61258a524dfc08738447f922ff4f9a4
SamuelCamarena <samuelcamarenacaballer@gmail.com>
2016-12-07 16:06:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-//app.set('port', (8080 || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/libs', express.static(__dirname + '/libs'));
+app.use('/images', express.static(__dirname + '/images'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
